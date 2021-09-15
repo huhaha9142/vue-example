620674f0ad2d16ff98e6de618ec1ec4606059a61
박상혁 <huhaha9142@naver.com>
2021-09-15 15:25:46 +0900

    Vue Router

--- sfc/src/Cart.vue
+++ sfc/src/Cart.vue
@@ -10,10 +10,10 @@
         </div>
         <shop-list v-bind:shopping-items="items"></shop-list>
         <cart-input v-on:add-item="addItem"></cart-input>
-        <input type="checkbox" v-model="showOption">구입한 물건 보기
-        <bought-list v-bind:bought-items="item"
-                     v-show="showOption"
-                     v-on:remove-item="removeItem"></bought-list>
+        <input type="checkbox" v-model="showOption">산 물건 보기
+        <bought-list v-bind:bought-items="items"
+            v-show="showOption"
+            v-on:remove-item="removeItem"></bought-list>
         <cart-footer></cart-footer>
     </div>
 </template>
@@ -26,12 +26,12 @@ import BoughtList from './components/BoughtList';
 import CartInput from './components/CartInput';
 export default {
     data: () => {
-        return {
+                return {
             items: [
-                { name: "무", buy: false},
-                { name: "배추", buy: false},
-                { name: "쪽파", buy: true},
-                { name: "고추가루", buy: true}
+                { name: "무", buy: false },
+                { name: "배추", buy: false },
+                { name: "쪽파", buy: true },
+                { name: "고춧가루", buy: true } 
             ],
             showAll: true,
             showOption: true
@@ -41,13 +41,12 @@ export default {
         "cart-header": CartHeader,
         "cart-footer": CartFooter,
         "shop-list": ShopList,
-        "bought-list": BoughtList
+        "bought-list": BoughtList,
         "cart-input": CartInput
-    }
-},
- methods: {
-    addItem: function(item) {
-        console.log("추가할 아이템:", item);
+    },
+    methods: {
+        addItem: function(item) {
+            console.log("추가할 아이템:", item);
             //  배열에 아이템 추가
             this.items.push({
                 name: item,
@@ -56,14 +55,14 @@ export default {
         },
         removeItem: function(item) {
             var index = this.items.indexOf(item);
-                if (index > -1) {   //  인덱스
-                    //  배열에서 요소 삭제
-                    this.items.splice(index, 1);
-                }
+            if (index > -1) {   //  인덱스
+                //  배열에서 요소 삭제
+                this.items.splice(index, 1);
             }
         }
+    }
+}
 </script>
-
 <style>
 li {
     list-style: none;
